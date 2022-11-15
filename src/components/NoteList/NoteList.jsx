2369159e7a6c0c69f60d131aaaa848137fcae730
Alexey Rusel <alexeyrusel@gmail.com>
2022-11-15 18:41:16 +0300

    feat: add adaptive design

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import classes from "./NoteList.module.css";
 import NoteItem from "../NoteItem/NoteItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const NoteList = ({ notes, remove, changeNoteStatus, mode }) => {
-    if (!notes.length) {
-        return <h1 className={classes.noNotes__title}>No Notes</h1>;
-    }
 
     return (
         <div className={classes.noteList}>
+            <TransitionGroup>
             {notes.map((note) => (
+                <CSSTransition
+                key={note.id}
+                timeout={400}
+                classNames='note'
+                >
                 <NoteItem
-                    key={note.id}
                     note={note}
                     remove={remove}
                     changeNoteStatus={changeNoteStatus}
                     mode={mode}
                 />
+                </CSSTransition>
             ))}
+            </TransitionGroup>
+            {!notes.length && <h1 className={classes.noNotes__title}>No Notes</h1>}
         </div>
     );
 };
